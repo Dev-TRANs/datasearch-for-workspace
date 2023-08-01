7663a304b02f977ddc79df7a0d59fcda802c7d6a
kombumori <dev.kombumori@gmail.com>
2023-08-01 16:55:32 +0900

    やっぱダサいなぁ()

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-function doGet() {
-    return HtmlService.createTemplateFromFile("index").evaluate().setSandboxMode(HtmlService.SandboxMode.IFRAME).setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
+function doGet(e) {
+    var page = e.parameter.page ? e.parameter.page : "index";
+    var pagesArray = ["index", "settings", "setup"];
+    if (pagesArray.includes(page)) {
+        return HtmlService.createTemplateFromFile(page).evaluate().setSandboxMode(HtmlService.SandboxMode.IFRAME).setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
+    } else {
+        return HtmlService.createTemplateFromFile("404").evaluate().setSandboxMode(HtmlService.SandboxMode.IFRAME).setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
+    }
+}
+
+function getWebAppURL() {
+    return ScriptApp.getService().getUrl()
 }
