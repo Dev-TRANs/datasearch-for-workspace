0dfa853236018226ee19cc08a05da0919e0e8b35
kombumori <dev.kombumori@gmail.com>
2023-08-01 16:02:46 +0900

    commit

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
 function doGet() {
     return HtmlService.createTemplateFromFile("index").evaluate().setSandboxMode(HtmlService.SandboxMode.IFRAME).setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
 }
-
-//4 test commit
